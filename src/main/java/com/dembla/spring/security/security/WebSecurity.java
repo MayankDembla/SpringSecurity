fea2e65e82bc44016a8c4433b6fcffcb6846735e
MayankDembla <43243575+MayankDembla@users.noreply.github.com>
2021-03-28 03:52:15 +0530

    #3 Role based Auth :  Add a Check of Role if developer... add a converter from jwt to grantAuthorities..

--- src/main/java/com/dembla/spring/security/security/WebSecurity.java
+++ src/main/java/com/dembla/spring/security/security/WebSecurity.java
@@ -1,15 +1,22 @@
 package com.dembla.spring.security.security;
 
+import com.dembla.spring.security.util.KeyCloakRoleConverter;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeyCloakRoleConverter());
+
+
         http.authorizeRequests()
                 .antMatchers(HttpMethod.GET,"/users/status/check")
                 // .hasAuthority("SCOPE_profile")
@@ -18,6 +25,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .oauth2ResourceServer()
-                .jwt();
+                .jwt()
+                .jwtAuthenticationConverter(jwtAuthenticationConverter) ;
     }
 }
