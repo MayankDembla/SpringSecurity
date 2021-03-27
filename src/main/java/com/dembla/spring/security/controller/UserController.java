5eef0f90e330e000cbed21f13e4453eb9fd86e4a
MayankDembla <43243575+MayankDembla@users.noreply.github.com>
2021-03-28 04:17:02 +0530

    #4 Method level Security :  @Secure with Role

--- src/main/java/com/dembla/spring/security/controller/UserController.java
+++ src/main/java/com/dembla/spring/security/controller/UserController.java
@@ -3,9 +3,8 @@ package com.dembla.spring.security.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -17,6 +16,12 @@ public class UserController {
     @GetMapping("/status/check")
     public String status(){
         return "working on port : " + env.getProperty("local.server.port") ;
+    }
+
+    @Secured("ROLE_developer")
+    @DeleteMapping("/delete/{id}")
+    public String delete(@PathVariable String id){
+        return "Deleted user with userid = " + id ;
     }
 
 }
--- src/main/java/com/dembla/spring/security/security/WebSecurity.java
+++ src/main/java/com/dembla/spring/security/security/WebSecurity.java
@@ -2,11 +2,13 @@ package com.dembla.spring.security.security;
 
 import com.dembla.spring.security.util.KeyCloakRoleConverter;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 
+@EnableGlobalMethodSecurity(securedEnabled = true)
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
