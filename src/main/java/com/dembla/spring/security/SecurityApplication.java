4ad7486ebc127e548bf4794592f4dbab879f54ae
MayankDembla <43243575+MayankDembla@users.noreply.github.com>
2021-03-30 05:01:33 +0530

    Add the Discovery Service

--- src/main/java/com/dembla/spring/security/SecurityApplication.java
+++ src/main/java/com/dembla/spring/security/SecurityApplication.java
@@ -2,9 +2,11 @@ package com.dembla.spring.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
-public class SecurityApplication {
+public class  SecurityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityApplication.class, args);
